29133765aee13b6470fd506e11423a8245a9489a
Jud Taylor <jud.taylor@protonmail.com>
2020-01-04 11:40:57 -0500

    Started chpter 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
+app/routes.py: Return complete HTML page from view function
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Jud'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello from Mars, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
