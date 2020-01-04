593ab107113863318180147a3a36f2385a61a00c
Jud Taylor <jud.taylor@protonmail.com>
2020-01-04 12:01:23 -0500

    Removed layout from routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Jud'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello from Mars, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
